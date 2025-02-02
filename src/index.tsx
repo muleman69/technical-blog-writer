e2b1d8691037154b1748e3eb506d285ffed9c1c1
muleman69 <jjresnick2020@gmail.com>
2025-02-01 19:37:01 -0800

    Sync local changes – update import and other fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './app/globals.css';
-import App from './app/page';
+import App from './app/page.tsx';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
